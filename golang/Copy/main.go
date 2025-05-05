6c133962c440b92459e4ac27c0a4297cdf79ce41
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-05-05 22:58:15 +0530

    copy

--- golang/Copy/main.go
+++ golang/Copy/main.go
@@ -1,1 +1,49 @@
 package main
+
+type Person struct {
+	Name string
+	Tags []string
+}
+
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Tags []string
+}
+
+// ShallowCopy
+func (p *Person) ShallowCopy() Person {
+	return *p // Note: Tags slice is shared
+}
+
+// DeepCopy
+func (p *Person) DeepCopy() Person {
+	// Copy the tags slice
+	tagsCopy := make([]string, len(p.Tags))
+	copy(tagsCopy, p.Tags)
+
+	return Person{
+		Name: p.Name,
+		Tags: tagsCopy,
+	}
+}
+
+func main() {
+	original := Person{
+		Name: "Abhi",
+		Tags: []string{"Go", "Developer"},
+	}
+
+	shallow := original.ShallowCopy()
+	deep := original.DeepCopy()
+
+	// Modify original's tags
+	original.Tags[0] = "Js"
+
+	fmt.Println("Original:", original)
+	fmt.Println("Shallow:", shallow)
+	fmt.Println("Deep:", deep)
+}
