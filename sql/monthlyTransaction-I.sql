f2216ea3a055f30ca68fe372991b4afc70c355ab
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-06-13 22:55:23 +0530

    monthly transaction - I

--- sql/monthlyTransaction-I.sql
+++ sql/monthlyTransaction-I.sql
@@ -1,6 +1,6 @@
 -- Write your MySQL query statement below
 
-SELECT date_format(trans_date, "%Y-%m") as month, country, count(state) as trans_count,
-SUM(if(state="approved", 1,0)) as approved_count, SUM(amount) as trans_total_amount,
-SUM(if(state="approved", amount, 0)) as approved_total_amount from Transactions
+SELECT date_format(trans_date, "%Y-%m") month, country, count(state) trans_count,
+SUM(if(state="approved", 1,0)) approved_count, SUM(amount) trans_total_amount,
+SUM(if(state="approved", amount, 0)) approved_total_amount from Transactions
 GROUP BY 1,2
