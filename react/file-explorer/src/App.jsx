c0abdfb24cc0e4c6f11fa619bf709826f404b5db
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-06-17 00:08:01 +0530

    file-explorer

--- react/file-explorer/src/App.jsx
+++ react/file-explorer/src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react"
+import json from "./data.json"
+
+// Render List of Objects
+const List = ({ list }) => {
+  const [isExpanded, setIsExpanded] = useState({})
+  return (<div className="container">
+    {list.map((node) => (
+      <div key={node.id}>
+        {node.isFolder && <span onClick={() => setIsExpanded((prev) => ({ ...prev, [node.name]: !prev[node.name] }))}>{isExpanded?.[node.name] ? "- " : "+ "}</span>}
+        <span>{node.name}</span>
+        {isExpanded?.[node.name] && node?.children && <List list={node.children} />}
+      </div>
+
+    ))}
+  </div>)
+}
+
 function App() {
+  const [data, setData] = useState(json)
   return (
     <div className="file-explorer">
-      <h1>File Explorer</h1>
+      <h1>File/Folder Explorer</h1>
+      <List list={data} />
     </div>
   )
 }
