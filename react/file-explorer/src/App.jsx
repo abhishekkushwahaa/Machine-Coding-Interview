36896edabe0a6c51d6c64e6353625cec500b417a
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-06-17 23:05:31 +0530

    file-explorer

--- react/file-explorer/src/App.jsx
+++ react/file-explorer/src/App.jsx
@@ -2,26 +2,65 @@ import { useState } from "react"
 import json from "./data.json"
 
 // Render List of Objects
-const List = ({ list }) => {
+const List = ({ list, addNodeToList, deleteNodeFromList }) => {
   const [isExpanded, setIsExpanded] = useState({})
   return (<div className="container">
     {list.map((node) => (
       <div key={node.id}>
         {node.isFolder && <span onClick={() => setIsExpanded((prev) => ({ ...prev, [node.name]: !prev[node.name] }))}>{isExpanded?.[node.name] ? "- " : "+ "}</span>}
         <span>{node.name}</span>
-        {isExpanded?.[node.name] && node?.children && <List list={node.children} />}
+        {node?.isFolder && (<span onClick={() => addNodeToList(node.id)}>
+          <img src="/add.png" alt="add" className="icon" />
+        </span>)}
+        <span onClick={() => deleteNodeFromList(node.id)}>
+          <img src="/delete.png" alt="delete" className="delete" />
+        </span>
+        {isExpanded?.[node.name] && node?.children && <List list={node.children} addNodeToList={addNodeToList} deleteNodeFromList={deleteNodeFromList} />}
       </div>
 
-    ))}
-  </div>)
+    ))
+    }
+  </div >)
 }
 
 function App() {
   const [data, setData] = useState(json)
+
+  const addNodeToList = (parentId) => {
+    const name = prompt("Enter Name")
+    const updateTree = (list) => {
+      return list.map(node => {
+        if (node.id === parentId) {
+          return {
+            ...node,
+            children: [...node.children, { id: Date.now().toString(), name: name, isFolder: true, children: [] }]
+          }
+        }
+        if (node.children) {
+          return { ...node, children: updateTree(node.children) }
+        }
+        return node;
+      })
+    }
+    setData((prev) => updateTree(prev))
+  }
+
+  const deleteNodeFromList = (itemId) => {
+    const updateTree = (list) => {
+      return list.filter((node) => node.id !== itemId).map((node) => {
+        if (node.children) {
+          return { ...node, children: updateTree(node.children) }
+        }
+        return node;
+      })
+    }
+    setData((prev) => updateTree(prev))
+  }
+
   return (
     <div className="file-explorer">
       <h1>File/Folder Explorer</h1>
-      <List list={data} />
+      <List list={data} addNodeToList={addNodeToList} deleteNodeFromList={deleteNodeFromList} />
     </div>
   )
 }
