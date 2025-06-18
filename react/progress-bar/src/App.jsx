98c8368778aeaedd637c07a4e2a8ea7f02d99479
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-06-18 23:08:12 +0530

    progress-bar

--- react/progress-bar/src/App.jsx
+++ react/progress-bar/src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+const ProgressBar = ({ progress }) => {
+
+  const [animatedProgress, setAnimatedProgree] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => setAnimatedProgree(progress), 100)
+  }, [progress])
+
+  return (
+    <div className="outer">
+      <div className="inner"
+        style={{
+          // width: `${progress}%`,
+          transform: `translateX(${animatedProgress - 100}%)`,
+          color: animatedProgress < 5 ? "black" : "white",
+        }}
+        role="progressbar"
+        aria-valuenow={progress}
+        aria-valuemax="100"
+        aria-valuemin="0"
+      > {progress}% </div>
+    </div >
+  )
+}
+
 function App() {
   return (
     <>
       <h1 className="title">Progress Bar</h1>
+      <ProgressBar progress={69} />
     </>
   )
 }
