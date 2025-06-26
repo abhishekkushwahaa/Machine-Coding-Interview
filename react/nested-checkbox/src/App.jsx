163976cac580a31849e3a7eff906110deb48b9a0
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-06-26 22:58:36 +0530

    nested checkbox

--- react/nested-checkbox/src/App.jsx
+++ react/nested-checkbox/src/App.jsx
@@ -1,3 +1,77 @@
+
+const checkBoxData = [
+  {
+    id: 1,
+    name: "Fruites",
+    children: [
+      {
+        id: 2,
+        name: "Summer",
+        children: [
+          {
+            id: 3,
+            name: "Orange"
+          },
+          {
+            id: 4,
+            name: "Mango"
+          }
+        ]
+      },
+      {
+        id: 5,
+        name: "Winter",
+        children: [
+          {
+            id: 6,
+            name: "Blackbarries"
+          },
+          {
+            id: 7,
+            name: "Strawbarries"
+          }
+        ]
+      },
+      {
+        id: 8,
+        name: "Others",
+        children: [
+          {
+            id: 9,
+            name: "Apple"
+          },
+          {
+            id: 10,
+            name: "Guava"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 11,
+    name: "Flowers",
+    children: [
+      {
+        id: 12,
+        name: "Rose"
+      }
+    ]
+  }
+]
+
+const CheckBoxes = ({ data }) => {
+  return <div>
+    {data.map((node) => (
+      <div className="parent" key={node.id}>
+        <input className="checkbox" type="checkbox" />
+        <span>{node.name}</span>
+        {node.children && <CheckBoxes data={node.children} />}
+      </div>
+    ))}
+  </div>
+}
+
 function App() {
   return (
     <>
@@ -5,6 +79,9 @@ function App() {
         <h2>
           Nested Checkboxes
         </h2>
+      </div>
+      <div className="checkbox-container">
+        <CheckBoxes data={checkBoxData} />
       </div>
     </>
   )
