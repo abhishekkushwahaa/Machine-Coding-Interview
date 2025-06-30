b909bf23f828b7bfa0a8d629ccb6e06379c3e528
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-06-30 23:18:41 +0530

    chips-input

--- react/chips-input/src/App.jsx
+++ react/chips-input/src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react"
+
 function App() {
+
+  const [inputText, setInputText] = useState("")
+
   return (
     <>
       <div className="chips-container">
         <h2>Chips Input</h2>
       </div>
       <div className="chips-input">
-        <input className="input-tag" type="text" placeholder="Type tags and chips" />
+        <input
+          className="input-tag"
+          type="text"
+          placeholder="Type tags and chips"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+      </div>
+      <div className="chips-input">
+        <span className="tags">{inputText}</span>
       </div>
     </>
   )
