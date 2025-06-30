2b24194a1e4d37d6e877f06460b0bca91b0ea9b1
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-06-30 23:12:40 +0530

    chips-input

--- react/chips-input/src/App.jsx
+++ react/chips-input/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
       <div className="chips-container">
         <h2>Chips Input</h2>
       </div>
+      <div className="chips-input">
+        <input className="input-tag" type="text" placeholder="Type tags and chips" />
+      </div>
     </>
   )
 }
