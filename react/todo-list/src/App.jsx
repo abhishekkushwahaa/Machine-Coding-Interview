2701e20b5827453913c3c4f289f66dcc0007fb5a
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-07-10 22:24:59 +0530

    todo-list

--- react/todo-list/src/App.jsx
+++ react/todo-list/src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from "react"
 
 function App() {
   const [input, setInput] = useState("")
+  const [todoList, setTodoList] = useState([
+    {
+      id: 1,
+      text: "React",
+      completed: true
+    }
+  ])
   return (
     <>
       <div className="todo-container">
@@ -11,6 +18,15 @@ function App() {
         <input className="todo-input" type="text" placeholder="Enter Todo"
           value={input} onChange={(e) => setInput(e.target.value)} />
         <button>Add</button>
+      </div>
+      <div className="todo-container">
+        <ul>
+          {todoList.map(t => <li>
+            <input type="checkbox" />
+            <span>{t.text}</span>
+            <button>Remove</button>
+          </li>)}
+        </ul>
       </div>
     </>
   )
