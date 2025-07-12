328705d88f8bae086a64fcc7d6d6b040c64100b7
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-07-12 22:28:01 +0530

    todo-list

--- react/todo-list/src/App.jsx
+++ react/todo-list/src/App.jsx
@@ -5,13 +5,39 @@ function App() {
   const [todoList, setTodoList] = useState([])
 
   const addTodoItem = () => {
+
+    if (input.trim() === "") return
+
     const item = {
-      id: 0,
-      text: input,
-      completed: true
+      id: todoList.length + 1,
+      text: input.trim(),
+      completed: false
     }
     setTodoList(prev => [...prev, item])
     setInput("")
+  }
+
+  const toggleCompleted = (id) => {
+    setTodoList(
+      todoList.map(t => {
+        if (t.id === id) {
+          return {
+            ...t,
+            completed: !t.completed
+          }
+        } else {
+          return t
+        }
+      })
+    )
+  }
+
+  const removeTodo = (id) => {
+    setTodoList(
+      todoList.filter(
+        (t) => (t.id !== id)
+      )
+    )
   }
 
   return (
@@ -27,12 +53,12 @@ function App() {
       <div className="todo-container">
         <ul>
           {todoList.map(t => <li key={t.id}>
-            <input type="checkbox" checked={t.completed} />
-            <span className="strikeThrough">{t.text}</span>
-            <button>Remove</button>
+            <input type="checkbox" checked={t.completed} onChange={() => toggleCompleted(t.id)} />
+            <span className={t.completed ? "strikeThrough" : ""}>{t.text}</span>
+            <button onClick={() => removeTodo(t.id)}>Remove</button>
           </li>)}
         </ul>
-      </div>
+      </div >
     </>
   )
 }
