613479d6fd6880b9857c53fef2c294ce4ab8ecc6
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-08-14 23:17:13 +0530

    accordion

--- react/accordion/src/App.jsx
+++ react/accordion/src/App.jsx
@@ -1,11 +1,29 @@
 function App() {
 
+  const items = [
+    {
+      title: "JavaScript",
+      content: "Nodejs, Reactjs, React Native, Nodejs, Nextjs etc."
+    },
+    {
+      title: "Golang",
+      content: "Mux, Gin, Concurrency etc."
+    },
+    {
+      title: "Python",
+      content: "Django, Flask, Streamlit etc."
+    }
+  ]
+
   return (
     <div className="heading-container">
       <h2>Accordion</h2>
       <div>
-        {items.map((item) => {
-          return <div></div>
+        {items.map((item, index) => {
+          return <div>
+            <button>{item.title}</button>
+            <div>{item.content}</div>
+          </div>
         })}
       </div>
     </div>
