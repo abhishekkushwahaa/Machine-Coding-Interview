9002d9f228fc2c801ffdc973b7c38e79282d9f94
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-08-20 22:58:09 +0530

    accordion

--- react/accordion/src/App.jsx
+++ react/accordion/src/App.jsx
@@ -1,29 +1,30 @@
+import { useState } from "react"
+
+const items = [
+  {
+    title: "JavaScript",
+    content: "Nodejs, Reactjs, React Native, Nodejs, Nextjs etc."
+  },
+  {
+    title: "Golang",
+    content: "Mux, Gin, Concurrency etc."
+  },
+  {
+    title: "Python",
+    content: "Django, Flask, Streamlit etc."
+  }
+]
+
 function App() {
-
-  const items = [
-    {
-      title: "JavaScript",
-      content: "Nodejs, Reactjs, React Native, Nodejs, Nextjs etc."
-    },
-    {
-      title: "Golang",
-      content: "Mux, Gin, Concurrency etc."
-    },
-    {
-      title: "Python",
-      content: "Django, Flask, Streamlit etc."
-    }
-  ]
-
+  const [openIndex, setOpenIndex] = useState(null)
   return (
     <div className="heading-container">
       <h2>Accordion</h2>
       <div className="accordion">
         {items.map((item, index) => {
-          return <div className="accordion-section">
+          return <div key={index} className="accordion-section">
             <button className="accordion-title">{item.title}</button>
-            <div className="accordion-content">{item.content}</div>
-          </div>
+            {openIndex === index && <div className="accordion-content">{item.content}</div>}          </div>
         })}
       </div>
     </div>
